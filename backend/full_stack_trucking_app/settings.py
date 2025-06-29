1a7175fb5e66e5958fbcf1b13951a0857cadb70f
George Herman Moshi <georgehermanmoshi@gmail.com>
2025-06-29 23:17:01 +0300

    Update settings.py

--- backend/full_stack_trucking_app/settings.py
+++ backend/full_stack_trucking_app/settings.py
@@ -100,5 +100,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://your-project-name.vercel.app",
+    "https://full-stack-trucking-app.onrender.com",
+    "https://full-stack-trucking-app.vercel.app/",
 ]
